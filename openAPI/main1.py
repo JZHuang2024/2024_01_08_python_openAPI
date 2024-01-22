7a9811d8b8a0a6371440b3862b911b46f855b1e3
JZHuang2024 <vivipic@outlook.com>
2024-01-22 13:09:31 +0800

    modify

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -33,13 +33,23 @@ async def get_item(date:str,celsius:float):
     print(f"溫度:{celsius}")
     return {"日期":date,"攝氏溫度":celsius}
 
+#
+#
 #2024.01.22.AM11:35
 #線上教學https://fastapi.tiangolo.com/tutorial/query-params/
-
+#
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
-#然後,進瀏覽器執行http://127.0.0.1:8000/docs
-#
+
+#▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+#此main.py必須在虛擬主機執行.指令:uvicorn main:app --reload
+#非按右上角 " ▷ " 執行
+#▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+#1.然後,進瀏覽器執行http://127.0.0.1:8000/docs
+#2.也可安裝 Thunder Client在Visual Studio Code進行無腳本測試
+#    Thunder Client 是用於 Visual Studio Code 的輕量級 Rest API 客戶端擴展，具有簡單易用的 UI。
+#    支持集合和環境變數和 GraphQL 查詢，以及使用基於 GUI 的界面進行無腳本測試。
+#    所有請求的資料都本地保存在您的設備上。
