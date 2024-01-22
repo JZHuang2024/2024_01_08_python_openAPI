f1e7ff93ffe8ce6c094ef7b39f7a80b9addeaa03
JZHuang2024 <vivipic@outlook.com>
2024-01-22 16:25:53 +0800

    modify

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -1,30 +1,3 @@
-#⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇this one run under Thonny⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
-#how to find the manual :
-#       MicroPython documentation /Quick reference for the RP2 / Pins and GPIO
-#       https://docs.micropython.org/en/latest/rp2/quickref.html#pins-and-gpio
-#⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
-
-#from machine import Pin
-#
-#red_led = Pin(15,mode=Pin.OUT)
-#red_led.value(1)
-#red_led.value(0)
-
-
-
-
-#from machine import Pin
-#import time
-
-#red_led = Pin(15,mode=Pin.OUT)
-#btn = Pin(14,mode=Pin.PULL_DOWN)
-
-#while True:
-#    if btn.value():
-#        red_led.value(1)
-#    else:
-#        red_led.value(0)
-
 from machine import Pin
 import time
 
