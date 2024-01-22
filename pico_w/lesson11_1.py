902fa86a059517b1b1f12bf1555aa2484a47ad8e
JZHuang2024 <vivipic@outlook.com>
2024-01-22 14:55:10 +0800

    MODIFY

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -1,3 +1,9 @@
+#⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇this one run under Thonny⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+#how find the manual :
+#       MicroPython documentation /Quick reference for the RP2 / Pins and GPIO
+#       https://docs.micropython.org/en/latest/rp2/quickref.html#pins-and-gpio
+#⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
+
 from machine import Pin
 
 red_led = Pin(15,mode=Pin.OUT)
