ae9b8a6424b9347b6df78388049dd229942dc8eb
JZHuang2024 <vivipic@outlook.com>
2024-01-24 11:43:06 +0800

    modify

--- openAPI/main2.py
+++ openAPI/main2.py
@@ -18,11 +18,6 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-async def get_item(item_id):
-    print(f"使用者輸入了:{item_id}")
-    return {"item_id":item_id}
-
-@app.get("/items/{item_id2}")
 async def get_item(item_id:int):
     print(f"使用者輸入了:{item_id}")
     return {"item_id":item_id}
@@ -33,26 +28,40 @@ async def get_item(date:str,celsius:float):
     print(f"溫度:{celsius}")
     return {"日期":date,"攝氏溫度":celsius}
 
-#
-#
-#2024.01.22.AM11:35
-#線上教學https://fastapi.tiangolo.com/tutorial/query-params/
-#
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
 
-#▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-#此main.py必須在虛擬主機執行.指令:uvicorn main:app --reload 或是(uvicorn main1:app --reload)(main????是測試檔)
+
+#2023.01.24.wed.AM11:30
+
+@app.get("/pico_w/{date}")
+async def read_item(date:str ,address:str,celsius:float,light:float):
+    print(f"日期:{date}")
+    print(f"位置:{address}")
+    print(f"攝氏:{celsius}")
+    print(f"光線:{light}")
+    return {"狀態":"儲存成功"}
+
+@app.get("/pico_w/{date}")
+async def read_item(date:str ,address:str,celsius:float=0.0):
+    print(f"日期:{date}")
+    print(f"位置:{address}")
+    print(f"攝氏:{celsius}")
+    return {"狀態":"儲存成功"}
+
+#▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+#此main.py必須在虛擬主機執行.指令:uvicorn main2:app --reload (main2????是測試檔)
 #非按右上角 " ▷ " 執行
-#▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+#▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 #1.然後,進瀏覽器執行http://127.0.0.1:8000/docs
 #2.也可安裝 Thunder Client在Visual Studio Code進行無腳本測試
 #    Thunder Client 是用於 Visual Studio Code 的輕量級 Rest API 客戶端擴展，具有簡單易用的 UI。
 #    支持集合和環境變數和 GraphQL 查詢，以及使用基於 GUI 的界面進行無腳本測試。
 #    所有請求的資料都本地保存在您的設備上。
-#    http://127.0.0.1:8000/pico_w/2024-01-22 16:02:10?address=chicken_KFC&celsius=15.386
-#    在下方輸入address     chicken_KFC
-#             celsius     15.386
+#    http://127.0.0.1:8000/pico_w/2024-01-24 11:19:00?address
+#    在下方輸入address     _KFC
+#             celsius     25.50
