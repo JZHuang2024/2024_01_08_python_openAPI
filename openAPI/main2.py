10a8ad6ed44a986c559f09ae1c4d251d1d5de291
JZHuang2024 <vivipic@outlook.com>
2024-01-24 12:22:23 +0800

    modify

--- openAPI/main2.py
+++ openAPI/main2.py
@@ -1,3 +1,5 @@
+2024_01_24_AM11:59
+
 #▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
 #此main.py必須在虛擬主機執行.指令:uvicorn main:app --reload
 #非按右上角 " ▷ " 執行
@@ -54,7 +56,7 @@ async def read_item(date:str ,address:str,celsius:float=0.0):
     return {"狀態":"儲存成功"}
 
 #▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-#此main.py必須在虛擬主機執行.指令:uvicorn main2:app --reload (main2????是測試檔)
+#此main2.py必須在虛擬主機執行.指令:uvicorn mainXXXX:app --reload (mainXXXX是測試檔)
 #非按右上角 " ▷ " 執行
 #▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 #1.然後,進瀏覽器執行http://127.0.0.1:8000/docs
@@ -62,6 +64,29 @@ async def read_item(date:str ,address:str,celsius:float=0.0):
 #    Thunder Client 是用於 Visual Studio Code 的輕量級 Rest API 客戶端擴展，具有簡單易用的 UI。
 #    支持集合和環境變數和 GraphQL 查詢，以及使用基於 GUI 的界面進行無腳本測試。
 #    所有請求的資料都本地保存在您的設備上。
-#    http://127.0.0.1:8000/pico_w/2024-01-24 11:19:00?address
-#    在下方輸入address     _KFC
-#             celsius     25.50
+#3.進入Thunder Client 在上方按下 New Request
+#    GET處輸入http://127.0.0.1:8000/pico_w/2024-01-24 11:36?2024-01-24 11:36? 
+#                                                                            ▲▲▲▲▲▲後面這段在Thunder Client下面輸入▲▲▲▲▲▲▲▲▲▲
+#    在下方輸入address     chicken_KFC
+#             celsius     26.6789
+#             light       45987
+#  或直接輸入 http://127.0.0.1:8000/pico_w/2024-01-24 11:36?2024-01-24 11:36?address=chicken_KFC&celsius=26.6789&light=45987
+#                                                                           
+#  按下 SEND 若成功出現下方
+#            
+#            {
+#             "狀態": "儲存成功"
+#            }
+#4. 下方伺服器成功會出現
+#        INFO:     Application startup complete.
+#        日期:2024-01-24 11:36
+#        位置:chicken_KFC
+#        攝氏:26.6789
+#        光線:45987.0
+#        INFO:     127.0.0.1:3986 - "GET /pico_w/2024-01-24%2011%3A36?address=chicken_KFC&celsius=26.6789&light=45987 HTTP/1.1" 200 OK
+#
+#
+#
+#
+#
+#
