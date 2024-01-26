ec597b08e3daf6b2b8de1b4caae0edd838a4210c
JZHuang2024 <vivipic@outlook.com>
2024-01-26 11:40:32 +0800

    modify

--- UI/main.py
+++ UI/main.py
@@ -1,5 +1,6 @@
 #2024_01_26_AM1110整合lesson12.ipynb 兩段程式
 #必須在終端機下的命令列執行 指令: python main.py
+'''
 import requests
 import pandas as pd
 
@@ -13,3 +14,30 @@ if r.status_code == 200:
 
 dataFrame = pd.DataFrame(data)
 print(dataFrame)
+'''
+#2024_01_26_AM1120 讓資料在網頁開啟
+#streamlit指令參考  https://streamlit.io/
+import requests
+import pandas as pd
+import streamlit as st
+
+url = 'https://openapi-test-6vnu.onrender.com/pico_w/?count=5'
+
+r = requests.get(url=url)
+
+if r.status_code == 200:
+    print("下載成功")
+    data = r.json()
+
+dataFrame = pd.DataFrame(data)
+
+st.header("學院養雞場")
+st.divider()
+st.caption("溫度-光線表表😍")
+st.write(dataFrame)
+st.divider()
+st.caption("光線")
+st.line_chart(dataFrame,x='date',y='light')
+st.divider()
+st.caption("溫度")
+st.line_chart(dataFrame,x='date',y='temperature',color='#ff0000')
